2eb2be03c4c262527d7d45e4efac22101221d179
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-02-09 10:52:42 +0530

    feat(scroll): load data on scroll

--- src/components/datapane/datapane.tsx
+++ src/components/datapane/datapane.tsx
@@ -1,38 +1,80 @@
-import {Component} from "react"
-import { Market } from "../../interface/market.interface"
-import MarketRow from "../market/market.component"
-import axios from "axios"
+import { Component } from "react";
+import { Market } from "../../interface/market.interface";
+import MarketRow from "../market/market.component";
+import axios from "axios";
 
-interface props{}
-interface state{
-    markets:Array<Market>
+/** declaring props */
+interface props {}
+
+/** declaring state structure */
+interface state {
+  markets: Array<Market>;
+  lock: boolean;
+  startIndex: number;
+  endIndex: number;
 }
 
-class DataPane extends Component<props,state> {
-    constructor(props:props){
-        super(props)
-        this.state = {
-            markets:[]
+/**
+ * DataPane to load and populate data
+ */
+class DataPane extends Component<props, state> {
+  /** initialize component state */
+  constructor(props: props) {
+    super(props);
+    this.state = {
+      markets: [],
+      lock: false,
+      startIndex: 0,
+      endIndex: 20,
+    };
+  }
+
+  /** function to run initial data */
+  async componentDidMount() {
+    const { data } = await axios.get("http://localhost:3000/hydrate");
+    this.setState({ markets: data });
+
+    window.addEventListener("scroll", async () => {
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        if (!this.state.lock) {
+          //   lock loading mechanism
+          this.setState({ lock: true });
+
+          //   load more details
+          const { endIndex } = this.state;
+          const { data } = await axios.get(
+            `http://localhost:3000/hydrate/${endIndex + 1}/${endIndex + 20}`
+          );
+          this.setState({
+            markets: [...this.state.markets, ...data],
+            endIndex: endIndex + 20,
+            lock: false,
+          });
         }
-    }
+      }
+    });
+  }
 
-    async componentDidMount(){
-        const {data}= await axios.get("http://localhost:3000/hydrate")
-        this.setState({markets:data})
-    }
+  /** to remove listener from scroll */
+  componentWillUnmount() {
+    window.removeEventListener("scroll", () => {
+      console.log("unmounted");
+    });
+  }
 
-    render(){
-        const {markets} = this.state
-        return (
-        <div>
-            <div className="p-4 flex-cols">
-                {
-                    markets.map((market:Market)=><MarketRow market={market} key={market.pair} />)
-                }
-            </div>
+  /** finally render items */
+  render() {
+    const { markets } = this.state;
+    return (
+      <div>
+        <div className="p-4 flex-cols">
+          {markets.map((market: Market) => (
+            <MarketRow market={market} key={market.pair} />
+          ))}
         </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
-export default DataPane
+export default DataPane;
--- src/components/market/market.component.tsx
+++ src/components/market/market.component.tsx
@@ -1,34 +1,29 @@
-import { Chip } from "../../interface/chip.interface"
-import ChipComponent from "../chip/chip.component"
-import {Component} from "react"
-import Divider from "../divider/divider.component"
-import { Market } from "../../interface/market.interface"
+import { Chip } from "../../interface/chip.interface";
+import ChipComponent from "../chip/chip.component";
+import { Component } from "react";
+import Divider from "../divider/divider.component";
+import { Market } from "../../interface/market.interface";
 
-interface props{
-    market:Market
+interface props {
+  market: Market;
 }
 
 class MarketRow extends Component<props> {
-    render(){
-        const {market} = this.props
-        console.log(market)
+  render() {
+    const { market } = this.props;
 
-        return (
-            <div className="flex flex-row">
-                {
-                    Object.keys(market.asks).map((chip, index)=>{
-                            return <ChipComponent data={market.asks[chip]} base={market.base} />
-                    })
-                }
-                <Divider title={market.pair}  />
-                {
-                    Object.keys(market.bids).map((chip, index)=>{
-                            return <ChipComponent data={market.bids[chip]} base={market.base} />
-                    })
-                }
-            </div>
-        )
-    }    
-} 
+    return (
+      <div className="flex flex-row">
+        {Object.keys(market.asks).map((chip, index) => {
+          return <ChipComponent data={market.asks[chip]} base={market.base} />;
+        })}
+        <Divider title={market.pair} />
+        {Object.keys(market.bids).map((chip, index) => {
+          return <ChipComponent data={market.bids[chip]} base={market.base} />;
+        })}
+      </div>
+    );
+  }
+}
 
-export default MarketRow
+export default MarketRow;
